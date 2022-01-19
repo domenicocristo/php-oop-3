589029943a57aa72ce0002448b533075b807a655
MyUsername <my@email.com>
2022-01-19 15:16:51 +0100

    add: user - computer

---
+++ Computer/index.php
@@ -0,0 +1,82 @@
+<?php
+class Computer {
+    private $codiceUnivoco;
+    private $modello;
+    private $prezzo;
+    private $marca;
+
+    public function __construct($codiceUnivoco, $prezzo) {
+        $this->setCodiceUnivoco($codiceUnivoco);
+        $this->setPrezzo($prezzo);
+    }
+
+    public function getCodiceUnivoco() {
+        return $this->codiceUnivoco;
+        
+    }
+
+    public function setCodiceUnivoco($codiceUnivoco) {
+        if (is_numeric($codiceUnivoco)!=6) {
+            throw new Exception("Il codice univoco deve contenere solo numeri e deve essere di 6 caratteri");
+        } else {
+            $this->codiceUnivoco = $codiceUnivoco;
+        }
+    }
+
+    public function getModello() {
+        return $this->modello;
+    }
+
+    public function setModello($modello) {
+        if (strlen($modello)<3 || strlen($modello)>20) {
+            throw new Exception("Il modello deve avere un minimo di 3 caratteri e un massimo di 20");
+        } else {
+            $this->modello = $modello;
+        }
+    }
+
+    public function getPrezzo() {
+        return $this->prezzo;
+    }
+
+    public function setPrezzo($prezzo) {
+        if (strlen($prezzo)<0 || strlen($prezzo)>2000) {
+            throw new Exception("Il prezzo deve essere un valore intero compreso tra 0 e 2000");
+        } else {
+            $this->prezzo = $prezzo;
+        }
+    }
+
+    public function getMarca() {
+        return $this->marca;
+    }
+
+    public function setMarca($marca) {
+        if (strlen($marca)<3 || strlen($marca)>20) {
+            throw new Exception("Il marca deve avere un minimo di 3 caratteri e un massimo di 20");
+        } else {
+            $this->marca = $marca;
+        }
+    }
+
+    public function printMe() {
+        echo $this . "<br>";
+    }
+
+    public function __toString() {
+        return $this->getMarca() . " " . $this->getModello() . ": " . $this->getPrezzo() . "$ [" . $this->getCodiceUnivoco() . "] ";
+    }
+}
+
+try {
+    $C1 = new Computer("123456", "3'000,00");
+
+    $C1->setMarca("Apple");
+
+    $C1->setModello("MacBook Pro");
+
+    $C1->printMe();
+} catch (Exception $e) {
+    echo $e . "<br><h3>" . $e->getMessage() . "</h3>";
+}
+?>
---
+++ User/index.php
@@ -0,0 +1,68 @@
+<?php
+class User {
+    private $username;
+    private $password;
+    private $age;
+
+    public function __construct($username, $password) {
+        $this->setUsername($username);
+        $this->setPassword($password);
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function setUsername($username) {
+        if (strlen($username)<5 || strlen($username)>15) {
+            throw new Exception("L'username deve avere minimo 5 caretteri e non può superare i 15");
+        } else {
+            $this->username = $username;
+        }
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function setPassword($password) {
+        if (preg_match("/[\'^£$%&*()}{@#~?><>,|=_+¬-]/", $this->password)) {
+            throw new Exception("La password deve contenere almeno un carattere speciale");
+        }
+        $this->password = $password;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setAge($age) {
+        if (!is_numeric($age)) {
+            throw new Exception("L'età deve essere un numero");
+        } else {
+            $this->age = $age;
+        }
+    }
+
+    public function printMe() {
+        echo $this . "<br>";
+    }
+
+    public function __toString() {
+        return $this->getUsername() . ": " . $this->getAge() . " [" . $this->getPassword() . "] ";
+    }
+}
+
+try {
+    $U1 = new User("Mario", "password1&&");
+    $U2 = new User("Francesca", "password2%%");
+
+    $U1->setAge("20");
+    $U2->setAge("21");
+
+    $U1->printMe();
+    $U2->printMe();
+} catch (Exception $e) {
+    echo $e . "<br><h3>" . $e->getMessage() . "</h3>";
+}
+?>
